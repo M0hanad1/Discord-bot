eb138bad44d56c6de10205111f968b8218a737ca
M0hanad1 <mohned.dabsha@gmail.com>
2022-02-06 14:17:38 +0200

    fixed topggpy problem with py-cord module

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
 import discord
+import topgg
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
 from src.prefix.prefix import Prefix
 
 
+load_dotenv()
 bot = commands.Bot(activity=discord.Game(name='+help'), intents=discord.Intents.all(), command_prefix=Prefix().get_prefix)
+bot.topggpy = topgg.DBLClient(bot, os.getenv('TOPGG_TOKEN'), autopost=True, post_shard_count=True)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is ready')
+
+
+@bot.event
+async def on_autopost_success():
+    print(f"Posted server count ({bot.topggpy.guild_count}), shard count ({bot.shard_count})")
 
 
 for folder in os.listdir('./src'):
@@ -19,5 +27,4 @@ for folder in os.listdir('./src'):
 
 
 if __name__ == '__main__':
-    load_dotenv()
     bot.run(os.getenv('TOKEN'))
